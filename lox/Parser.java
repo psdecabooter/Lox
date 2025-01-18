33670616bca0001ede5d23094d38ac8a635f0b53
Patrick DeCabooter <psdecabooter@gmail.com>
2025-01-18 09:13:47 -0600

    Onto ch 9

--- lox/Parser.java
+++ lox/Parser.java
@@ -43,6 +43,7 @@ public class Parser {
 
     private Stmt statement() {
         if(match(PRINT)) return printStatement();
+        if (match(LEFT_BRACE)) return new Stmt.Block(block());
 
         return expressionStatement();
     }
@@ -69,6 +70,17 @@ public class Parser {
         Expr expr = expression();
         consume(SEMICOLON, "Expect ';' after expression.");
         return new Stmt.Expression(expr);
+    }
+
+    private List<Stmt> block() {
+        List<Stmt> statements = new ArrayList<>();
+
+        while (!check(RIGHT_BRACE) && !isAtEnd()) {
+            statements.add(declaration());
+        }
+
+        consume(RIGHT_BRACE, "Expect '}' after block.");
+        return statements;
     }
 
     private Expr assignment() {
